8366b0e02ad3f0fdf75e5c128953003d5bd85d6d
calebdeji <calebdeji06@gmail.com>
2019-08-07 09:21:08 +0100

    Added error message to promise error

--- sw.js
+++ sw.js
@@ -4,11 +4,15 @@ const cacheList = ['./', './index.html', './css/', './js/', './image/favi.png'];
 self.addEventListener("install", (event) => {
     event.waitUntil(caches.open(cacheName).then((cache) => {
         return cache.addAll(cacheList);
+    }).catch((err) => {
+        console.log("Error Adding Cache : ", err);
     }));
 });
 
 self.addEventListener("fetch", (event) => {
     event.respondWith(caches.match(event.request).then((response) => {
         return response || fetch(event.request);
+    }).catch((err) => {
+        console.log("Error fecthing cache : ", err);
     }));
 })
